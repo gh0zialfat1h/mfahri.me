327ad6dd71421ebb2abc9cfa28e4d01ec25f4281
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-28 09:34:52 +0000

    Deploy gh0zialfat1h/muhammadfahri.me to github.com/gh0zialfat1h/muhammadfahri.me.git:gh-pages

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bc82cbede80fa185b192.js"
+    "url": "webpack-runtime-d0d3edce68af2a07fb7d.js"
   },
   {
     "url": "framework-a38040c4749cf445737f.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3d77a4dfb2abbd43cd12297fb61bbc67"
+    "revision": "cf205088185cc2691fcbc9abd56b1fe8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9875e2473a7c518755f.js"
